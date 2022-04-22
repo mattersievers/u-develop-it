f757d848549715fcb56a9ea36acb784aa6cf132e
Matthew Sievers <MatterSievers@gmail.com>
2022-04-22 12:02:45 -0500

    added SQL to readme

--- routes/apiRoutes/partyRoutes.js
+++ routes/apiRoutes/partyRoutes.js
@@ -19,7 +19,7 @@ router.get('/parties', (req,res) => {
 
 //get a single party
 router.get('/party/:id', (req,res) => {
-    const sql = `SELECT * FROM parties WHERE id-?`;
+    const sql = `SELECT * FROM parties WHERE id=?`;
     const params = [req.params.id];
     db.query(sql, params, (err,row) => {
         if(err){
